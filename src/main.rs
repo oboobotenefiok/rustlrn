2d681b9205f020c1b342b95a288abc4518a24a1a
Obot Obo <oboobotenefiok@gmail.com>
2026-04-21 18:01:40 +0100

    Update main.rs
    Trust me, I'll be doing a lot of testing so don't trust my commit messages, lol. Autocorrect said it's vomit messages 😂

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+/* Spectra, Obot here... feel we can make this a CLI tool for learning Rust so I'll add actual stuff to main and we can iterate from there. Think it will be cool that way? I'm not versed in the contribution culture but for now this comment should be fine :-) We can learn a lot from implementing this and fuse it to an app or web later when we understand better...*/
+
+fn main() {
+
+mod start;
+
+start::start_here();
+
+
+
+}
